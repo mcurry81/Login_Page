552962dc69ebd4b74a253bfdafb47b70e081db08
Melissa Curry <mcurry4@mail.greenriver.edu>
2015-03-14 20:03:05 -0700

    Create login_success.php
    This login page was intended for a group project (basApp). This page should display the database we created as well as other features for admin use.

---
+++ login_success.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+//define session variables to equal what post variable inputs are
+$_SESSION['username'] = $_POST['username'];
+$_SESSION['password'] = $_POST['password'];
+
+//prints username in welcome message and has links to logout and to changepassword
+if ($_SESSION['username']){
+	echo "Welcome, ".$_SESSION['username']."!<br><a href = 'logout.php'>Logout</a><br><a href = 'changepassword.php'>Change Password</a><br><br> ";
+	echo "The database would be here if this page were really connected to a database.";
+}
+else 
+{
+	die("You must be logged in");
+}
+
+?>
